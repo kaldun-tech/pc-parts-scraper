c94df266e70ba0a7f3e03c838a7c1fcd38c59340
Taras <tsmereka@protonmail.com>
2025-01-19 14:34:31 -0600

    CDK init

---
+++ tests/unit/test_pc_parts_scraper_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from pc_parts_scraper.pc_parts_scraper_stack import PcPartsScraperStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in pc_parts_scraper/pc_parts_scraper_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = PcPartsScraperStack(app, "pc-parts-scraper")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
