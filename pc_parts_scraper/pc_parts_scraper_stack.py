f86bcab3d38984ee01cd07d01f65ca203e65dc94
Taras <tsmereka@protonmail.com>
2025-01-19 16:10:28 -0600

    Added parameter store

--- pc_parts_scraper/pc_parts_scraper_stack.py
+++ pc_parts_scraper/pc_parts_scraper_stack.py
@@ -1,19 +1,25 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ssm as ssm,
 )
 from constructs import Construct
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class PcPartsScraperStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PcPartsScraperQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # ParameterStore details for Discord webhook URL
+        discord_webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
+        string_param = ssm.StringParameter(
+            self,
+            "DiscordWebhookUrl",
+            parameter_name="DISCORD_WEBHOOK_URL",
+            string_value=discord_webhook_url,
+            description="Discord Webhook URL String",
+            tier=ssm.ParameterTier.STANDARD
+        )
